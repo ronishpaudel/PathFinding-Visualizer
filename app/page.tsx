f07a0a68ceae9b2ad4a4daf7f8564657c4cd3e28
robuGdada <paudelronish@gmail.com>
2024-12-06 09:00:28 +0545

    fix mobile responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import PathfindingVisualizer from "@/components/pathFinidingVisualizer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-5 md:p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-[5px] sm:p-5 md:p-24">
       <h1 className="text-4xl font-bold mb-8">Pathfinding Visualizer</h1>
       <PathfindingVisualizer />
       <AlgorithmInfo />
